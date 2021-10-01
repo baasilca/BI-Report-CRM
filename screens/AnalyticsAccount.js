0ca51c19fb4d3fbed03311cfb7610f7d7939edb9
ashikh <ashikhwyd098@gmail.com>
2021-10-01 14:18:57 +0530

    style

--- screens/AnalyticsAccount.js
+++ screens/AnalyticsAccount.js
@@ -35,13 +35,9 @@ const AnalyticsAccount = () => {
                             console.log(selectedItem, index)
                         }}
                         buttonTextAfterSelection={(selectedItem, index) => {
-                            // text represented after item is selected
-                            // if data array is an array of objects then return selectedItem.property to render after item is selected
                             return selectedItem
                         }}
                         rowTextForSelection={(item, index) => {
-                            // text represented for each item in dropdown
-                            // if data array is an array of objects then return item.property to represent item in dropdown
                             return item
                         }}
                     />
